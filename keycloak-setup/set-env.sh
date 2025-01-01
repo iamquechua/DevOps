ed42f6069761f62c3515084f04917dcb99385e58
iamquechua <douno30@proton.me>
2025-01-01 13:42:47 +0000

    Update set-env.sh

--- keycloak-setup/set-env.sh
+++ keycloak-setup/set-env.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-export KC_HOSTNAME_URL=https://keycloak-shiv.duckdns.org
+export KC_HOSTNAME_URL=https://iam.dounotech.com
 export POSTGRES_USER=keycloak
 export POSTGRES_DB=keycloak
 export POSTGRES_PASSWORD=Password123
 export KEYCLOAK_ADMIN_PASSWORD=Password123
-export KEYCLOAK_FRONTEND_URL=https://keycloak-shiv.duckdns.org/auth
+export KEYCLOAK_FRONTEND_URL=https://iam.dounotech.com/auth
 export KEYCLOAK_ADMIN=admin
-export KEYCLOAK_HOSTNAME=keycloak-shiv.duckdns.org
+export KEYCLOAK_HOSTNAME=iam.dounotech.com
 #export SMTP_SERVER=
 #export SMTP_PORT=
 #export SMTP_FROM=
